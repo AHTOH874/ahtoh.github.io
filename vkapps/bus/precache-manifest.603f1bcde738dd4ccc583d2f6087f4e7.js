b59e340e29fe9075bc9e1287d3a431ec6947a41d
AHTOH874 <melhov.tosha@gmail.com>
2019-04-06 00:43:32 +0300

    bus 0.0.1
    Vk Apps

---
+++ vkapps/bus/precache-manifest.603f1bcde738dd4ccc583d2f6087f4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db1a8cc98c846cf52a0f",
+    "url": "/vkapps/bus/static/js/runtime~main.db1a8cc9.js"
+  },
+  {
+    "revision": "9e121e3c4f1943bd2842",
+    "url": "/vkapps/bus/static/js/main.9e121e3c.chunk.js"
+  },
+  {
+    "revision": "e274a7f8840b89b4a773",
+    "url": "/vkapps/bus/static/js/2.e274a7f8.chunk.js"
+  },
+  {
+    "revision": "e274a7f8840b89b4a773",
+    "url": "/vkapps/bus/static/css/2.862cf2e3.chunk.css"
+  },
+  {
+    "revision": "a065d56b92ac42e93d85942edca40f8a",
+    "url": "/vkapps/bus/index.html"
+  }
+];
