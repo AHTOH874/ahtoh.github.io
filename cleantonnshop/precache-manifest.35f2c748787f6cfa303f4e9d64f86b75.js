3b4482664207c869db93119fd4d032eb645e7508
AHTOH874 <melhov.tosha@gmail.com>
2019-05-11 23:07:20 +0300

    shop

---
+++ cleantonnshop/precache-manifest.35f2c748787f6cfa303f4e9d64f86b75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3cf4ceb4b2e461a4410e173da7f7c87",
+    "url": "/cleantonnshop/index.html"
+  },
+  {
+    "revision": "ecbee7933fb2411421d5",
+    "url": "/cleantonnshop/static/css/2.5dd5b3a2.chunk.css"
+  },
+  {
+    "revision": "bf4b67c805ab0d1fdf9c",
+    "url": "/cleantonnshop/static/css/main.553ab3a8.chunk.css"
+  },
+  {
+    "revision": "ecbee7933fb2411421d5",
+    "url": "/cleantonnshop/static/js/2.0e7cdb45.chunk.js"
+  },
+  {
+    "revision": "bf4b67c805ab0d1fdf9c",
+    "url": "/cleantonnshop/static/js/main.e38b4e72.chunk.js"
+  },
+  {
+    "revision": "130ffea98736cabc7b56",
+    "url": "/cleantonnshop/static/js/runtime~main.7436acef.js"
+  },
+  {
+    "revision": "3b46cf11f6959340bf9414b2e9e70822",
+    "url": "/cleantonnshop/static/media/GLAVTEXT.3b46cf11.png"
+  },
+  {
+    "revision": "1d80a6e94b9846e2fc72740722a099a1",
+    "url": "/cleantonnshop/static/media/swiss721bt-italic.1d80a6e9.ttf"
+  }
+]);
